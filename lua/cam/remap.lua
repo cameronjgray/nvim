888c46d613ee65e31b856836b896250f1783108a
Cameron Gray <cameron@procuros.io>
2023-04-12 15:26:09 +0100

    prettier map

--- lua/cam/remap.lua
+++ lua/cam/remap.lua
@@ -24,7 +24,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- paste without copy
-vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("x", "<leader>bp", [["_dP]])
 
 -- replace all occurences
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
@@ -32,3 +32,6 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- center next
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+
+-- prettier
+vim.keymap.set("n", "<leader>ff", ":! npx prettier -w <C-R>%<CR><CR>")
