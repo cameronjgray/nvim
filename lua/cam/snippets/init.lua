b4e85966728de9892f316937234cefe8fef2e0ef
Cameron Gray <this@cameronjgray.com>
2025-11-18 08:58:03 +0000

    feat(snippets): added some new helpful snippets

--- lua/cam/snippets/init.lua
+++ lua/cam/snippets/init.lua
@@ -1,1 +1,3 @@
-vim.keymap.set("n", "<leader>,api",":-1r $HOME/.config/nvim/lua/cam/snippets/api-template<CR> 3j2wcw" )
+vim.keymap.set("n", "<leader>,api",":-1r $HOME/.config/nvim/lua/cam/snippets/api-template<CR> gg_2wcw" )
+vim.keymap.set("n", "<leader>,sql",":-1r $HOME/.config/nvim/lua/cam/snippets/sql-setup<CR> gg/table1<CR>cw" )
+vim.keymap.set("n", "<leader>,comp",":-1r $HOME/.config/nvim/lua/cam/snippets/react-component<CR> :%s/Component/" )
