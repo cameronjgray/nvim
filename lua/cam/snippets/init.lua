0d79eecb2450892b9c3ca5f0143ccb41f4c4ce2a
Cameron Gray <this@cameronjgray.com>
2025-11-18 09:09:26 +0000

    feat(snippets): added docker snippets

--- lua/cam/snippets/init.lua
+++ lua/cam/snippets/init.lua
@@ -1,3 +1,5 @@
 vim.keymap.set("n", "<leader>,api",":-1r $HOME/.config/nvim/lua/cam/snippets/api-template<CR> gg_2wcw" )
 vim.keymap.set("n", "<leader>,sql",":-1r $HOME/.config/nvim/lua/cam/snippets/sql-setup<CR> gg/table1<CR>cw" )
 vim.keymap.set("n", "<leader>,comp",":-1r $HOME/.config/nvim/lua/cam/snippets/react-component<CR> :%s/Component/" )
+vim.keymap.set("n", "<leader>,dockerc",":-1r $HOME/.config/nvim/lua/cam/snippets/docker-compose<CR> gg" )
+vim.keymap.set("n", "<leader>,dockerf",":-1r $HOME/.config/nvim/lua/cam/snippets/docker-file<CR> gg" )
