dc65f5f94ccd543ccaf2f6ed16d0b73da1d086be
Cameron Gray <this@cameronjgray.com>
2026-04-02 16:38:40 +0100

    chore(dadbod): updated config

--- lua/config/plugins/dadbod.lua
+++ lua/config/plugins/dadbod.lua
@@ -1,5 +1,23 @@
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
+  { "tpope/vim-dadbod" },
+  { "kristijanhusak/vim-dadbod-completion" },
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    init = function()
+      vim.g.vim_dadbod_completion_mark_as_keyword = 0
+      vim.g.omni_sql_no_default_maps = 1
+      vim.g.db_ui_auto_fold_results = 0
+    end,
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "dbout",
+        callback = function()
+          vim.notify("dbout autocmd fired")
+          vim.opt_local.foldenable = false
+          vim.opt_local.foldmethod = "manual"
+        end,
+      })
+    end,
+  },
 }
+
